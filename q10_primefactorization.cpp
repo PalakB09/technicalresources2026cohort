c5a981012b9b2f1a52c70f1c0ced537b3c0e28f8
PalakB09 <117338984+PalakB09@users.noreply.github.com>
2023-01-18 19:45:03 +0530

    submitting q10 of assignment

---
+++ q10_primefactorization.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num; 
+    cin >> num;
+    for (int i=2;i<=num;i++)
+    {
+            while (num%i==0)
+            {
+                cout << i<<"\n";
+                num=num/i;
+            }
+    }
+    return 0;
+}
